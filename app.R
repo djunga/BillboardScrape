589de6cbed06cf5c18e080c837d86f78eeecee66
djunga <20993137+djunga@users.noreply.github.com>
2024-07-07 15:58:12 -0400

    Add max and min points, change title panel CSS

--- app.R
+++ app.R
@@ -5,7 +5,8 @@ library(tidyverse)   # lubridate included
 library(ragg)
 library(showtext)
 
-#data <- read.csv("audio_features_calendar.csv")
+#setwd("C:/Users/pebbl/Desktop/Shiny R Practice/MyShinyWebApp")
+
 trend_data <- read.csv("https://raw.githubusercontent.com/djunga/BillboardScrape/main/avg_trends.csv")
 trend_data <- trend_data %>%
   mutate(month = factor(month, levels = month.name))
@@ -31,52 +32,64 @@ calendar_data_choices <- list(
   tempo = tempo_data
 )
 
+# function for converting the month column in the metric dataframes to factor
+transform_month_factor <- function(df) {
+  df <- df %>% 
+        mutate(month = factor(month, levels = month.name),
+               weekday = factor(weekday, levels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
+              )
+  return (df)
+}
 
-metric_choices <- list(
-  energy = c("avg_energy", energy_data),
-  valence = c("avg_valence", valence_data),
-  loudness = c("avg_loudness", loudness_data),
-  speechiness = c("avg_speechiness", speechiness_data),
-  acousticness = c("avg_acousticness", acousticness_data),
-  instrumentalness = c("avg_instrumentalness", instrumentalness_data),
-  liveness = c("avg_liveness", liveness_data),
-  tempo = c("avg_tempo", tempo_data)
-)
+# Custom CSS for title panel
+custom_css <- "
+.title-panel {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icon-links {
+  display: flex;
+  gap: 20px;
+}
+.icon-links a {
+  color: white;
+  font-size: 1.5em;
+}
+"
 
 # Define UI
 ui <- page_sidebar(
-  title = "Billboard - Spotify Audio Features Dashboard",
-
+  title =
+    div(
+      class = "title-panel",
+      "Billboard Hot 100™ - Spotify Audio Features",
+      div(
+        class = "icon-links",
+        a(href = "https://github.com/djunga", icon("github"), target = "_blank"),
+        a(href = "https://www.linkedin.com/in/toram1", icon("linkedin"), target = "_blank")
+      )
+    ),
     # Dropdown box for selecting a choice
     sidebar = sidebar(
-                selectInput("selected_feature", "Select variable for Y-axis:",
+                selectInput("selected_feature", "Select Spotify Audio Feature:",
                 choices = names(calendar_data_choices),
                 selected = names(calendar_data_choices)[1])
                 ),
 
   navset_card_tab(
     nav_panel("Features Scatterplot", plotOutput("scatterPlot")),
-    nav_panel("Calendar", plotOutput("heatmap"))
+    nav_panel("Calendar Heatmap", plotOutput("heatmap"))
   ),
+  tags$head(
+    tags$style(HTML(custom_css))
+  )
 )
-
-# ui <- fluidPage(
-#   titlePanel = "Billboard - Spotify Audio Features Dashboard", 
-#   sidebarLayout(
-#     sidebarPanel(
-#     # Dropdown box for selecting a choice
-#     selectInput("selected_feature", "Select variable for Y-axis:",
-#                 choices = names(calendar_data_choices), 
-#                 selected = names(calendar_data_choices)[1]),
-#   
-#     ),
-#   navset_card_tab(
-#     nav_panel("Features Scatterplot", plotOutput("scatterPlot")),
-#     nav_panel("Calendar", plotOutput("heatmap"))
-#   ),
-#   ),
-# )
-
 
 
 # color ramp
@@ -117,15 +130,26 @@ theme_calendar <- function(){
 server <- function(input, output) {
   output$scatterPlot <- renderPlot({
     selected_col <- paste0("avg_", input$selected_feature)
-    ggplot(trend_data, mapping=aes(x = month, y = .data[[selected_col]])) +
+
+    max_point <- trend_data[which.max(trend_data[[selected_col]]), ]
+    min_point <- trend_data[which.min(trend_data[[selected_col]]), ]
+    
+    ggplot(trend_data, mapping=aes(x = month, y = .data[[selected_col]], group=1)) +
       geom_point(size = 3) +
-      labs(x = "Month", y = input$selected_col) +
-      theme_minimal()
+      geom_point(data=max_point, mapping=aes(x=month, y=.data[[selected_col]]), size = 6, pch=17, color="red") + 
+      geom_point(data=min_point, mapping=aes(x=month, y=.data[[selected_col]]), size = 6, pch=17, color="blue") + 
+      #annotate("text", x = max_point$month, y = max_point[[selected_col]], label = "Some text") +
+      geom_line(linetype = "dashed") +
+      labs(title = paste0("Average ", stringr::str_to_title(input$selected_feature), " By Month"),
+          x = "Month", y = paste0("Average ", stringr::str_to_title(input$selected_feature))) +
+      theme_classic()
   })
   
   output$heatmap <- renderPlot({
     selected_col <- input$selected_feature
-    ggplot(data = calendar_data_choices[[selected_col]], aes(weekday, -week, fill = pcat)) +
+    
+    df <- transform_month_factor(calendar_data_choices[[selected_col]])
+    ggplot(data = df, aes(weekday, -week, fill = pcat)) +
       geom_tile(colour = "white", size = .4)  + 
       geom_text(aes(label = day, colour = text_col), size = 2.5) +
       guides(fill = guide_colorsteps(barwidth = 25, 
@@ -135,10 +159,10 @@ server <- function(input, output) {
                         na.value = "grey90", drop = FALSE) +
       scale_colour_manual(values = c("black", "white"), guide = FALSE) + 
       facet_wrap(~month, nrow = 4, ncol = 3, scales = "free") +
-      labs(title = "Average Energy Per Week", 
+      labs(title = paste0("Average ", stringr::str_to_title(selected_col), " Per Week"), 
            subtitle = "Billboard 100, 2023",
-           caption = "Data: Spotify",
-           fill = "avg energy") +
+           caption = "Data: Spotify Developer API",
+           fill = paste0("avg ", selected_col)) +
       theme_calendar()
   })
 }
